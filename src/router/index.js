d3e4eba60440e58537f10ba9baeb3a86dff59389
howardmed8 <hmedinapi@unadvirtual.edu.co>
2025-08-29 17:21:55 +0000

    rgfdhdfd

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from '../pages/AppHome.vue'
-import AppContact from '../pages/AppContact.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: AppHome
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: AppContact
   }
 ]
 
